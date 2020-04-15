c4a20ec897c6a40340f209beac227f42c04f57c6
Jon <jon@jonadams.co.uk>
2020-04-15 15:21:39 +0100

    Switched back to HashRouter as GH Pages doesn't seem to play well with normal BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route } from "react-router-dom";
+import { HashRouter, Route } from "react-router-dom";
 
 import App from "./App";
 import Home from "./Home";
 
 ReactDOM.render(
-    <BrowserRouter>
+    <HashRouter>
         <App>
             <Route exact path="/:url?" component={Home} />
         </App>
-    </BrowserRouter>,
+    </HashRouter>,
 
     document.getElementById("root")
 );
