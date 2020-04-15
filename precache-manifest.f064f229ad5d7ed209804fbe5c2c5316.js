734ce2fbcb6c8d9ccc62b5983e4e434180550817
Jon <jon@jonadams.co.uk>
2020-04-15 14:20:30 +0100

    Updates

---
+++ precache-manifest.f064f229ad5d7ed209804fbe5c2c5316.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7f34280d2afd509f6571d8b3ba19683",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66dd3bbd139f25cdc1f8",
+    "url": "/static/css/main.8625bae9.chunk.css"
+  },
+  {
+    "revision": "cb2f4289a510c1245287",
+    "url": "/static/js/2.36edab9b.chunk.js"
+  },
+  {
+    "revision": "98cac36a2dc526a417c8340008b123a7",
+    "url": "/static/js/2.36edab9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66dd3bbd139f25cdc1f8",
+    "url": "/static/js/main.d65db40a.chunk.js"
+  },
+  {
+    "revision": "d5219476db3e1885a3dc",
+    "url": "/static/js/runtime-main.94d97cc0.js"
+  },
+  {
+    "revision": "7c30b16e3994681604e8ddfbb0944c4f",
+    "url": "/static/media/logo-large.7c30b16e.png"
+  }
+]);
